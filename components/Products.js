f217e01dabb6925040c2abb39d8fecfc9cc78c22
Vida Feng <vidaffeng@gmail.com>
2023-03-06 16:56:29 -0500

    product page cards

--- components/Products.js
+++ components/Products.js
@@ -1,20 +1,39 @@
 import React from "react";
 import Image from "next/image";
 import { data } from "../utils/data";
+import { Grid, GridItem, Card, Box } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Products = () => {
   return (
     <div>
-      {data.products.map((product) => (
-        <Image
-          key={product.id}
-          src={`/images${product.image}`}
-          alt={product.title}
-          height={230}
-          width={230}
-          style={{ objectFit: "cover", height: "230px" }}
-        />
-      ))}
+      <Grid
+        templateColumns={{
+          base: "1fr",
+          lg: "repeat(4, 1fr)",
+        }}
+        gap={6}
+      >
+        {data.products.map((product) => (
+          <GridItem key={product.id} colSpan={1}>
+            <Card>
+              <Link href={`/product/${product.id}`}>
+                <Image
+                  src={`/images${product.image}`}
+                  alt={product.title}
+                  height={230}
+                  width={230}
+                  style={{ objectFit: "cover", height: "230px" }}
+                />
+                <Box py={1} px={2}>
+                  <h3>{product.title}</h3>
+                  <div>${product.price}</div>
+                </Box>
+              </Link>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
     </div>
   );
 };
