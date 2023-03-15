974cbf8a2526953b987c43ad370b5e346a48596d
Vida Feng <vidaffeng@gmail.com>
2023-03-15 14:41:55 -0400

    cart reducer

--- context/Cart.js
+++ context/Cart.js
@@ -4,10 +4,39 @@ const initialState = {
   cart: [],
 };
 
-
 const cartReducer = (state, action) => {
-    switch(action.type) {
-        case 'ADD_ITEM':
-            return {...state, cart: [...state.cart, ]}
-    }
-}
+  switch (action.type) {
+    case "ADD_ITEM":
+      return { ...state, cart: [...state.cart, action.payload] };
+
+    case "REMOVE_ITEM":
+      return {
+        ...state,
+        cart: [...state.cart.filter((item) => item.id !== action.payload)],
+      };
+    default:
+      return state;
+  }
+};
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  const addToCart = (item) => {
+    dispatch({ type: "ADD_ITEM", payload: item });
+  };
+
+  const removeFromCart = (itemId) => {
+    dispatch({ type: "REMOVE_ITEM", payload: itemId });
+  };
+
+  return (
+    <CartContext.Provider value={{ ...state, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { CartProvider, CartContext };
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
+import { CartProvider } from "../context/Cart";
 
 export default function App({ Component, pageProps }) {
   // layout in app.js instead of index.js to have on all pages
   return (
     <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <CartProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CartProvider>
     </ChakraProvider>
   );
 }
