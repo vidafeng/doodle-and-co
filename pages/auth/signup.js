56f495f9432b56312d38323f6551de21fe725983
Vida Feng <vidaffeng@gmail.com>
2023-03-22 22:23:56 -0400

    sign up api

---
+++ pages/auth/signup.js
@@ -0,0 +1,56 @@
+import db from "../../utils/db";
+import { hashPassword } from "../../utils/auth";
+import User from "../../models/User";
+
+async function handler(req, res) {
+  // method should be 'POST' -- secure
+  if (req.method !== "POST") {
+    return;
+  }
+
+  const data = req.body;
+
+  const { name, email, password } = data;
+
+  // validation on backend
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message: "Invalid Input",
+    });
+    return;
+  }
+
+  await db.connect();
+
+  //   check if user exists
+  const existingUser = await User.findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({
+      message: "This email address is already in use",
+    });
+    db.disconnect();
+    return;
+  }
+
+  //   hash password
+  //   go to utils auth.js
+  const hashedPassword = await hashPassword(password);
+
+  const result = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "New user created successfuly" });
+
+  await db.disconnect();
+}
+
+export default handler;
--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,12 @@
 import jwt from "jsonwebtoken";
+import { hash } from "bcryptjs";
+
+export const hashPassword = async (password) => {
+  if (password.length < 8) {
+    throw new Error("Password must be at least 8 characters");
+  }
+  return await hash(password, 12);
+};
 
 export const signToken = (user) => {
   return jwt.sign(
