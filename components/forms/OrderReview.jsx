75a392f28f28828a65217093e0335f7e3f7d6dde
Vida Feng <vidaffeng@gmail.com>
2023-03-29 17:23:27 -0400

    order review pages

--- components/forms/OrderReview.jsx
+++ components/forms/OrderReview.jsx
@@ -12,7 +12,44 @@ export const OrderReview = () => {
 
     const shippingPrice = subtotal > 50 ? 0 : 7.99
     const tax = subtotal * 0.08875
-    const subtotal = cart.reduce((acc, item) => acc + item.price, 0)
+    const subtotal = cart.reduce((acc, item) => acc + (item.price * item.qty), 0)
     const total = subtotal + tax + shippingPrice
 
+    return (
+    <Stack divier={<StackDivider/>} spacing='4'>
+        <Box>
+            <Heading size='xs' textTransform='uppercase'>Shipping Address</Heading>
+
+            <Text pt='2' fontSize='sm'>{state.shippingAddress.fullName}</Text>
+        
+            <Text pt='2' fontSize='sm'>{state.shippingAddress.address}</Text>
+            
+            <Text pt='2' fontSize='sm'>{state.shippingAddress.city}</Text>
+            
+            <Text pt='2' fontSize='sm'>{state.shippingAddress.postalCode}</Text>
+            
+            <Text pt='2' fontSize='sm'>{state.shippingAddress.country}</Text>
+            
+        </Box>
+
+        <Box>
+            <Heading size='xs' textTransform='uppercase'>Payment Method</Heading>
+            <Text>
+                {state.shippingAddress.paymentMethod ? state.shippingAddress.paymentMethod :"Paypal"}
+            </Text>
+        </Box>
+
+        <Box>
+            <Heading size='xs' textTransform='uppercase'>Order Summary</Heading>
+            <Text pt='2' fontSize='sm'>Items: ${subtotal}</Text>
+            <Text pt='2' fontSize='sm'>Shipping: ${shippingPrice}</Text>
+            <Text pt='2' fontSize='sm'>Tax: ${tax}</Text>
+            <Text pt='2' fontSize='sm'>Total: ${total}</Text>
+
+            <Flex justify='center' align='center' pt='4'>
+                <Button colorScheme='yellow' size='sm'>Place Order</Button>
+            </Flex>
+        </Box>
+    </Stack>
+    )
 }
